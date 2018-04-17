11b6956340e5ee609bcdb8e1d1aec121198807b7
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-04-17 13:34:21 -0500

    added smooth scrolling, changed color scheme

---
+++ js/personal-site-js.js
@@ -0,0 +1,33 @@
+"use strict";
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+};
+
+$('a[href*="#"]').click(function(event) {
+// On-page links
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+            } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+            };
+        });
+    }
+});
+
+$('#header').scroll()
